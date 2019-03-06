e400818195811fd255123a5d488a5046f59336ed
Kristina Ivanova <45533932+kristinaiv992@users.noreply.github.com>
2019-03-06 23:56:56 +0100

    Update Program.cs

--- 3. do While loop/do-while/do-while/Program.cs
+++ 3. do While loop/do-while/do-while/Program.cs
@@ -47,6 +47,42 @@ namespace do_while
             Console.WriteLine($"From your {numbers} numbers {counterforzeros} are devidible with three");
             Console.WriteLine($"From your {numbers} numbers {counterforones} have a residue of 1 while dividing with 3");
             Console.WriteLine($"From your {numbers} numbers {counterforones} have a residue of 2 while dividing with 3");
+            
+            //---------------Task 2: (DO WHILE) -------------------------------//
+            
+            
+            
+            
+            
+            
+            //---------------Task 3: (DO WHILE) -------------------------------//
+                 // Program that counts all the even numbers to an entered number:
+            string choice = string.Empty;
+            do
+            {
+                Console.WriteLine("Please enter a number");
+                int input = int.Parse(Console.ReadLine());
+
+                int start = 0;
+
+                while (start < input)
+                {
+                    Console.Write(start + " ");
+                    start += 2;
+                }
+                do
+                {
+                    Console.WriteLine("Do you want to continue - Yes or No");
+                    choice = Console.ReadLine().ToUpper();
+                    if (choice != "YES" && choice != "NO")
+                    {
+                        Console.WriteLine("You did not entered yes or no please repeat Yes or No");
+                    }
+                }
+                while (choice != "YES" && choice != "NO");
+            }
+            while (choice == "YES");
+            
         }
     }
 }
